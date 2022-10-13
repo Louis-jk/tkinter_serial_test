3bf23bf3a323d100c9759fb61b3c1f8ef246a1f4
sin3107 <s0pp3107@gmail.com>
2022-10-13 15:19:24 +0900

    printing test

--- simplegui.py
+++ simplegui.py
@@ -1,17 +1,70 @@
 import tkinter
 from tkinter.ttk import Combobox
-
 from escpos.printer import Serial
-p = Serial(devfile='/dev/tty.Bluetooth-Incoming-Port',
-           baudrate=9600,
-           bytesize=8,
-           parity='N',
-           stopbits=1,
-           timeout=1.00,
-           dsrdtr=True)
 
-p.text("Hello World\n")
-p.qr("You can readme from your smartphone")
+# layout 
+width = 35
+currency = 'currency'
+quantity = 'quantity'
+product = 'item'
+shot_name = 'DONGNAEBOOk'
+
+# receipt
+def receipt(purchases):
+    total = 0
+
+    items = [        
+        shot_name.center(width),
+        product.ljust(width),
+        currency.rjust(width)
+    ]
+
+    for name, price, count in purchases:
+        total += price*count
+
+        all_price = str(round(price*count, 2))
+
+        msg = f'{name}'.ljust(width-len(all_price), ' ')
+        msg += all_price
+
+        if type(count) is int and count >= 2:
+            msg += f'\n     {count} kg x {price}'
+        elif type(count) is float:
+            msg += f'\n     {count} kg x {price}'
+
+        items.append(msg)
+    total = str(round(total, 2))
+    items.append("\nTOTAL:".ljust(width-len(total)+1) + total)
+
+    return '\n'.join(items)
+
+ITEMS = [
+    ('Bananas', 1.15, 3500),
+    ('Bananas', 1.15, 3500),
+    ('Bananas', 1.15, 3500),
+    ('Bananas', 1.15, 3500),
+    ('Bananas', 1.15, 3500),
+]
+
+text = receipt(ITEMS)
+print(text)
+        
+
+
+# p = Serial(devfile='/dev/tty.Bluetooth-Incoming-Port',
+#            baudrate=9600,
+#            bytesize=8,
+#            parity='N',
+#            stopbits=1,
+#            timeout=1.00,
+#            dsrdtr=True)
+
+# cp949
+p = Serial('COM4', 9600)
+p.charcode('CP932') 
+p.text("동네북 테스트\n")
+p.text(text)
+p.qr("https://www.naver.com/")
 p.cut()
 
 window = tkinter.Tk()
@@ -31,4 +84,4 @@ baudRateValues=[110, 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
 combobox = Combobox(window, height=15, values=baudRateValues)
 combobox.pack()
 
-window.mainloop()
+# window.mainloop()
